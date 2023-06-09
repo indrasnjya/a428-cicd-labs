fa6e308aa7e68f625ef1410c6d555f490d98de7d
Moh Indra sanjaya <135502733+indrasnjya@users.noreply.github.com>
2023-06-10 00:09:23 +0700

    Update deliver.bat

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,5 +1,13 @@
+@echo off
+
 REM Build the application
 npm run build
 
+REM Start the application
+start "" npm start
+
+REM Wait for the application to start
+ping 127.0.0.1 -n 10 > nul
+
 REM Finish
-echo "Delivery completed."
+echo Application deployed successfully.
