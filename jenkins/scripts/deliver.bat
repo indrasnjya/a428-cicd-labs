e6c4122d9b78e338935cca67d472b88c2e2996af
Moh Indra sanjaya <135502733+indrasnjya@users.noreply.github.com>
2023-06-10 01:08:16 +0700

    Update deliver.bat

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,7 +1,5 @@
-#!/bin/bash
-
-# Copy index.html to public directory
+REM Copy index.html to public directory
 copy src\index.html public\index.html
 
-# Finish
+REM Finish
 echo "Delivery completed."
